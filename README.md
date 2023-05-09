425100641a36741b4368540044f25fb77f051e44
Sumanth Aluri <sumanth.phi2000@gmail.com>
2023-05-09 17:11:46 -0400

    A branch to implement an OpenAI demo

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # VanillaJS-PWA
 A simple Progressive Web App made with pure JS, HTML, and CSS.
 No external libraries, just the fundamentals
+
+## To run:
+Since this app uses the fetch api to make an https requests we need to serve it from a server in order to avoid CORS request errors.
+The simplest way to do this is by simply starting an https server using python in the root directory (where index.html is).
+* open terminal
+* navigate to root directory
+* `python -m https.server`
--- app.js
+++ app.js
@@ -1,184 +1,131 @@
+// import {secret} from "./secrets.js";
 const line1 = document.querySelector('#line1');
-const line2 = document.querySelector('#line2');
-const line3 = document.querySelector('#line3');
-const checkBtn = document.querySelector('#check');
-const saveBtn = document.querySelector('#save');
+
+const saveBtn = document.querySelector('#summarize');
 const clearBtn = document.querySelector('#clear');
 const main = document.querySelector('main');
 const line1hint = document.getElementById('line1hint');
-const line2hint = document.getElementById('line2hint');
-const line3hint = document.getElementById('line3hint');
 
-// const url1 = "https://api.wordnik.com/v4/word.json/";
-// const url2 = "/hyphenation?useCanonical=false&limit=50&api_key="
-// const key = "70538348db6b42e43a5181e32070feebc0b303e293ed13a97" // some other key
-///const key = "7r89in7oqdhkuoea2xcx1mu9w4rj5eaat3qdlqeyxjobf76n9" /// Make sure to remove this !!!!!
+
 line1.focus();
-let freeTester;
-let freeTester2;
 
-/// indexdb Setup
-let database
-window.onload = function () {
-    ///requests to open v1 of user_haikus database
-    let request = window.indexedDB.open('haikus', 1);
-    request.onsuccess = function () {
-        console.log('Database opened');
-        database = request.result;
-        showOld();
-    };
-    request.onerror = function () {
-        console.log('RIP database did not open')
-    }
-    request.onupgradeneeded = function (e) {
-        database = e.target.result;
-        ///create an object in the data base,
-        /// basically like a table
-        let table = database.createObjectStore('user_haikus', { keyPath: 'id', autoIncrement: true });
-        table.createIndex('lines', 'line1', { unique: false });
-        console.log('Database setup done');
-    }
+let api_key;
+
+window.onload = function ()
+{
+    //load secret key
+    fetch('./secret.json')
+    .then(response => response.json())
+    .then(data => {
+        api_key = data.api_key
+    })
+    .catch(error => console.log(error));
 }
 
 
-////function definitions:
-async function showOld() {
-    console.log("read started")
-    let transaction = database.transaction(['user_haikus'], 'readwrite')
-    let objectStore = transaction.objectStore('user_haikus')
-    objectStore.getAll().onsuccess = function (e) {
-        // console.log("got these from the db: " + JSON.stringify(e.target.result))
-        e.target.result.map(element => {
-            createBlock(element["lines"])
-        });
-    }
-}
-
-
-function displayUHaikus() {
-    console.log("Consider haikus displayed");
-    createBlock([line1.value, line2.value, line3.value]);
-    clear();
-};
-function createBlock(lines) {
+function createBlock(content) {
     const spacer = document.createElement('br');
     const block = document.createElement('div');
     const l1 = document.createElement('p');
-    const l2 = document.createElement('p');
-    const l3 = document.createElement('p');
-    l1.textContent = lines[0];
-    l2.textContent = lines[1];
-    l3.textContent = lines[2];
+
+    l1.textContent = content;
 
     block.classList.add("bluck");
     console.log(l1.value);
 
     main.appendChild(spacer);
     block.appendChild(l1);
-    block.appendChild(l2);
-    block.appendChild(l3);
     main.appendChild(block);
 };
-function check() {
-    console.log("checking")
-    let yes = (
-        (sylbCount(line1.value)=== 5)
-        &&(sylbCount(line2.value)=== 7)
-        &&(sylbCount(line3.value)=== 5)
-        );
-    console.log(yes)
-    return yes
-}
+
 
 function updatehint(){
-    let line1c = sylbCount(line1.value)
-    line1hint.style = "color: gray";
-    if(line1c === 5){
-        line1hint.style = "color: rgb(0, 255, 0);";
+    let input = line1.value
+    var wordCount = input.match(/(\w+)/g).length;
+    line1hint.style = "color: rgb(0, 255, 0)";
+    if(!check(input)){
+        line1hint.style = "color: rgb(255, 0, 0);";
     }
-    line1hint.innerHTML = `${line1c}`;
-
-    let line2c = sylbCount(line2.value)
-    line2hint.style = "color: gray";
-    if(line2c === 7){
-        line2hint.style = "color: rgb(0, 255, 0);";
-    }
-    line2hint.innerHTML = `${line2c}`;
-
-    let line3c = sylbCount(line3.value)
-    line3hint.style = "color: gray";
-    if(line3c === 5){
-        line3hint.style = "color: rgb(0, 255, 0);";
-    }
-    line3hint.innerHTML = `${line3c}`;
+    line1hint.innerHTML = `${wordCount}`;
 }
 
  function clearhint(){
     line1hint.innerHTML = ``;
-    line2hint.innerHTML = ``;
-    line3hint.innerHTML = ``;
- }
+ };
 
-function sylbCountHelper(word) { /// bruh = {i, words}
-    ///This code was taken from Blake Tarter
-    ///https://codepen.io/blaketarter/pen/hJICz
-    if (word.length === 0) { return 0; }
-    word = word.toLowerCase();
-    if (word.length <= 3) { return 1; }
-    word = word.replace(' ', '&#13');
-    word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
-    word = word.replace(/^y/, '');
-    return word.match(/[aeiouy]{1,2}/g).length;
+
+function setwarning(warning){
+    console.log(warning)
 }
 
-function sylbCount(line) {
-    line = line.toLowerCase();
-    let words = line.split(" ");
-    let count = 0
-    words.forEach(element => {
-        count += sylbCountHelper(element)
-    });
-    return count;
+function OpenaiFetchAPI(input) {
+    console.log("Calling GPT3")
+    secrete_key = api_key//secret.getKey()
+
+    var url = "https://api.openai.com/v1/completions";
+    var bearer = 'Bearer ' + secrete_key
+    summary = fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': bearer,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "prompt": input + "\n\nTl;dr",
+            "model" : "text-davinci-003", 
+            "max_tokens": 150,
+            "temperature": 0.7,
+            "top_p": 1,
+            "n": 1,
+            "stream": false,
+            "logprobs": null,
+            "stop": "\n"
+        })
+
+
+    }).then(response => {
+        return response.json()
+       
+    }).then(data=>{
+        console.log(data)
+        console.log(typeof data)
+        console.log(Object.keys(data))
+        console.log(data['choices'][0].text)
+        return data['choices'][0].text
+    }).catch(error => {
+            console.log('Something bad happened ' + error)
+     });
+
+    return summary
 }
 
-function save(e) {
+function check(input){
+    //TODO: should check if number of tokens is within limit
+    var wordCount = input.match(/(\w+)/g).length;
+    return wordCount < 750
+}
+
+function summarize(e) {
     e.preventDefault();
-    //check();
-    if (check()) {
-        let newItem = { lines: [line1.value, line2.value, line3.value] };
-        /// open a transaction, get ready to read/write data
-        let transaction = database.transaction(['user_haikus'], 'readwrite');
-        /// create an object to be stored
-        let storeunit = transaction.objectStore('user_haikus');
-        let request = storeunit.add(newItem); /// maybe t should be storeunit?
-        transaction.onerror = function () {
-            console.log('Rip, transaction error');
-        }
-        transaction.oncomplete = function () {
-            console.log("New User Haiku added");
-            displayUHaikus();
-        }
-        transaction.onerror = function (e) {
-            console.log("transaction failed")
-            console.log(e)
-        }
+    input = line1.value
+    if (true) { // can add a check here to limit requests that are too large
+        summary = OpenaiFetchAPI(input)
+        summary.then(summ => {
+            createBlock(summ)
+        })
+    }else{
+        setwarning("Input too large!")
     }
 }
 
+
 function clear() {
     line1.value = "";
-    line2.value = "";
-    line3.value = "";
-    
 }
 
-checkBtn.addEventListener('click', check);
-saveBtn.addEventListener('click', save);
+saveBtn.addEventListener('click', summarize);
 clearBtn.addEventListener('click', clear);
 line1.addEventListener("input", updatehint);
-line2.addEventListener("input", updatehint);
-line3.addEventListener("input", updatehint);
-
 // <!--
 
 // Permission is hereby granted, free of charge, to any person 
